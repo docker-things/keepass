748506df259886524d73cfa11d4513adb800e7f9
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2019-08-23 15:10:27 +0300

    Increase memory limit and fix volume paths

--- config.sh
+++ config.sh
@@ -12,11 +12,11 @@ RUN_ARGS=(
     -e DISPLAY=$DISPLAY
     -v /tmp/.X11-unix:/tmp/.X11-unix
 
-    --memory="128m"
+    --memory="256m"
     --cpu-shares=1024
 
-    -v $(pwd)/data/db/MY_DB_FILE.kdbx:/home/$(whoami)/db/keepass.kdbx
-    -v $(pwd)/data/db/MY_KEY_FILE:/home/$(whoami)/db/key
+    -v $(pwd)/data/db:/home/$(whoami)/data/db
+    -v $(pwd)/data/key:/home/$(whoami)/data/key
     -v $(pwd)/data/config/keepassxc:/home/$(whoami)/.config/keepassxc
     -v $(pwd)/data/config/QtProject.conf:/home/$(whoami)/.config/QtProject.conf
 
